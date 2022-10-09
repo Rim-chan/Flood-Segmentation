69a913fe938c53465560642754ee70ad31ba40dd
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-10-09 17:43:16 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -3,19 +3,18 @@ import numpy as np
 import pytorch_lightning as pl
 import segmentation_models_pytorch as smp
 from monai.networks.nets import DynUNet, AttentionUnet
-
-
 from losses import *
 from metrics import *
+import mlflow
 
 class Unet(pl.LightningModule):
-    def __init__(self, config, train_dataloader):
+    def __init__(self, args, train_dataloader):
         super().__init__()
-        self.config = config
+        self.args = args
         self.train_dataloader = train_dataloader
         self.build_model()
         self.loss = LossFlood()
-        self.dice = DiceFlood(n_class=self.config.out_channels)
+        self.dice = DiceFlood(n_class=self.args.out_channels)
         
     
     def training_step(self, batch, batch_idx):
@@ -36,8 +35,8 @@ class Unet(pl.LightningModule):
         preds = (nn.Sigmoid()(preds) > 0.5).int()
         lbl_np = lbl.detach().cpu().numpy()
         preds_np = preds.detach().cpu().numpy()
-        np.save(self.config.save_path + 'predictions.npy', preds_np)
-        np.save(self.config.save_path + 'labels.npy', lbl_np) 
+        np.save(self.args.save_path + 'predictions.npy', preds_np)
+        np.save(self.args.save_path + 'labels.npy', lbl_np) 
 
     def training_epoch_end(self, outputs):
         torch.cuda.empty_cache()
@@ -50,7 +49,13 @@ class Unet(pl.LightningModule):
         
         print(f"Val_Performace: dice_mean {dice_mean:.3f}, Val_Loss {loss.item():.3f}")
         self.log("dice_mean", dice_mean)
-        self.log("Val_Loss", loss.item())           
+        self.log("Val_Loss", loss.item())   
+        
+        mlflow.log_metrics({
+        "dice_mean": dice_mean,
+        "Val_Loss": loss.item()
+        })
+        
         torch.cuda.empty_cache()
         gc.collect()        
         
@@ -59,19 +64,19 @@ class Unet(pl.LightningModule):
         self.model = smp.Unet(
             encoder_name="timm-mobilenetv3_small_minimal_100",  # resnet18/resnet34/mobilenet_v2/efficientnet-b7
             encoder_weights=None,  #"imagenet"
-            in_channels=self.config.in_channels,
-            classes=self.config.out_channels,
+            in_channels=self.args.in_channels,
+            classes=self.args.out_channels,
             decoder_attention_type=None)  #'scse'
         
         
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(),
-                                     lr=self.config.learning_rate,
-                                     weight_decay=self.config.weight_decay)
+                                     lr=self.args.learning_rate,
+                                     weight_decay=self.args.weight_decay)
         
         scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,
-                                                        max_lr=self.config.max_lr,
-                                                        epochs=self.config.num_epochs,
+                                                        max_lr=self.args.max_lr,
+                                                        epochs=self.args.num_epochs,
                                                         steps_per_epoch=len(self.train_dataloader))
         
         scheduler = {"scheduler": scheduler, "step" : "step" } 

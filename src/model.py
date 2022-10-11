77ad5b164c153c47bbb3dd7242434280c39909a2
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-10-11 09:16:38 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -95,7 +95,7 @@ class Unet(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(),
                                      lr=self.args.learning_rate,
-                                     weight_decay=self.args.weigh_decay)
+                                     weight_decay=self.args.weight_decay)
         
        
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,

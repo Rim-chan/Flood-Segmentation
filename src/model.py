46daa80092ba88ac72a00c520481a37946c6ea3f
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-10-11 15:15:24 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -63,7 +63,8 @@ class Unet(pl.LightningModule):
         mlflow.log_params({
         "batch_size": self.args.batch_size,
         "LR": self.args.learning_rate,
-        "Optimizer": type(self.configure_optimizers()[0]).__name__
+        "Optimizer": type(self.configure_optimizers()[0][0]).__name__,
+        "Scheduler": type(self.configure_optimizers()[1][0]['scheduler']).__name__
         })
   
         mlflow.pytorch.log_model(self.model, "model")
